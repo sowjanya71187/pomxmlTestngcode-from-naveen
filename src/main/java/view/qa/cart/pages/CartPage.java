ea557da6df00a4ee85222e327b1870377e126088
sowjanya71187 <sowji.gadiraju@gmail.com>
2021-03-12 19:17:57 +0530

    on cart branch

--- src/main/java/view/qa/cart/pages/CartPage.java
+++ src/main/java/view/qa/cart/pages/CartPage.java
@@ -1,9 +1,15 @@
 package view.qa.cart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
+	private By cart = By.id("cart");
 	
 	public void cartpage() {
 		System.out.println("this is my cart page");
+	}
+	public void clickoncart() {
+		System.out.println("click on cart"+cart);
 	}
 
 }
