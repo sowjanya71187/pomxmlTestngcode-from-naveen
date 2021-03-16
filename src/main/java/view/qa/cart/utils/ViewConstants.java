00d209086f49ee78130ae956c8cb8af816f3c41f
sowjanya71187 <sowji.gadiraju@gmail.com>
2021-03-16 18:42:30 +0530

    udate code in resources

--- src/main/java/view/qa/cart/utils/ViewConstants.java
+++ src/main/java/view/qa/cart/utils/ViewConstants.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ViewConstants {
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACC_PAGE_TITLE = "My Account";
-	public static final int HEADER_PAGE_COUNT = 4;
+	public static final int HEADER_PAGE_COUNT = 5;
 	public static final int PRODUCT_IMAGES_COUNT = 4;
 	public static final String ACCOUNT_CREATION_SUCCESS_MESSG = "Your Account Has Been Created!";
 	//********************************SheetNames***************************
